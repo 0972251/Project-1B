cb00d6ea27fffa9e71b4e6b25f746963ed2c993a
428Eyp <57940329+428Eyp@users.noreply.github.com>
2020-03-03 12:29:25 +0100

    test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Project_1B
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! World!");
         }
     }
 }
